0af34a56f85b6c94670ff756430c8654442cb526
conaldev <talaaidoaibiet@gmail.com>
2020-11-04 15:58:39 +0700

    tao xong category edit component html ts logic

--- src/app/questions/category/category-edit/category-edit.component.ts
+++ src/app/questions/category/category-edit/category-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {ActivatedRoute, Router} from "@angular/router";
+import {CategoryService} from "../../../service/category.service";
+import {ICategory} from "../ICategory";
 
 @Component({
   selector: 'app-category-edit',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryEditComponent implements OnInit {
 
-  constructor() { }
+  updateCategoryForm: FormGroup;
 
+
+  constructor(private router: ActivatedRoute, private categoryService: CategoryService, private formBuilder: FormBuilder, private route: Router) {
+  }
+
+  currentCategory: ICategory;
   ngOnInit(): void {
+    // tslint:disable-next-line:radix
+    const id = Number.parseInt(this.router.snapshot.paramMap.get('id'));
+    this.categoryService.findById(id).subscribe(data => {
+      this.currentCategory = data;
+    });
+    this.updateCategoryForm = this.formBuilder.group({
+      category: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit() {
+    // @ts-ignore
+    console.log(this.currentBook);
+    this.categoryService.update( this.currentCategory.id, this.currentCategory).subscribe(e => {
+      console.log(e);
+    });
+    this.route.navigate(['']);
   }
 
 }
