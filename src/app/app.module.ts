cfbbe5ec6cad12b2022b4a1b58c2044ea3cfc038
Boolshyt <chakeriototo@gmail.com>
2020-11-04 09:35:33 +0700

    create account component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,8 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {InputSimpleComponent} from './input-simple/input-simple.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {TableComponent} from './table/table.component';
 import {DemoNavComponent} from './demo-nav/demo-nav.component';
@@ -12,17 +10,18 @@ import {LayoutModule} from '@angular/cdk/layout';
 import {FooterComponent} from './footer/footer.component';
 import {LoginComponent} from './login/login.component';
 import {MaterialsModule} from "./materials/materials.module";
-import { LayoutWithSharedComponent } from './layout-with-shared/layout-with-shared.component';
+import {LayoutWithSharedComponent} from './layout-with-shared/layout-with-shared.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InputSimpleComponent,
     TableComponent,
     DemoNavComponent,
     FooterComponent,
     LoginComponent,
     LayoutWithSharedComponent,
+    CreateAccountComponent,
   ],
   imports: [
     MaterialsModule,
