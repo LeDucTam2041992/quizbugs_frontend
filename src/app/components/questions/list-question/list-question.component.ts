b08b4dcd5903dc14710749dbe4367e90ee7baa3e
conaldev <talaaidoaibiet@gmail.com>
2020-11-05 20:34:57 +0700

    them material list question, ket noi api

---
+++ src/app/components/questions/list-question/list-question.component.ts
@@ -0,0 +1,64 @@
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
+import {MatTableDataSource} from "@angular/material/table";
+import {CategoryService} from "../../../service/category.service";
+import {Router} from "@angular/router";
+import {QuestionService} from "../../../service/question.service";
+
+@Component({
+  selector: 'app-list-question',
+  templateUrl: './list-question.component.html',
+  styleUrls: ['./list-question.component.css']
+})
+export class ListQuestionComponent implements OnInit {
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+  listQuestions: MatTableDataSource<any>;
+  displayedColumns = ['id', 'question','status','type','category', 'option'];
+  searchKey: string;
+
+  constructor(private questionService: QuestionService, private router: Router) {
+  }
+
+  ngOnInit(): void {
+    this.getAll()
+  }
+
+  onSearchClear() {
+    this.searchKey = "";
+    this.applyFilter();
+  }
+
+  applyFilter() {
+    this.listQuestions.filter = this.searchKey.trim().toLowerCase();
+  }
+
+  getAll() {
+    this.questionService.getAllQuestion().subscribe(list => {
+        let array = list.map(item => {
+          return {
+            id: item.id,
+            question: item.question,
+            type: item.type,
+            status: item.status,
+            category: item.category,
+            answers: item.answers
+          }
+        });
+        this.listQuestions = new MatTableDataSource(array);
+        this.listQuestions.paginator = this.paginator;
+        this.listQuestions.sort = this.sort;
+      }
+    )
+  }
+
+  delete(id) {
+    if (confirm('Are you sure want to delete?')) {
+      this.questionService.deleteQuestion(id).subscribe(res => {
+        this.getAll();
+      })
+    }
+  }
+}
