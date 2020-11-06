8394c1f9d333c02122aabf2f7d8fe5615660016d
tamleduc <Sakursaoyran204>
2020-11-06 15:05:51 +0700

    ban truoc merger template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-
-import {LayoutWithSharedComponent} from "./layout-with-shared/layout-with-shared.component";
-import {CategoryModule} from "./components/category/category.module";
-// import {CreateQuestionComponent} from "./components/questions/create-question/create-question.component";
-import {LoginComponent} from "./components/login/login.component";
-import {CreateAccountComponent} from "./components/create-account/create-account.component";
-import {QuestionModule} from "./components/questions/question.module";
-// import {ListQuestionComponent} from "./components/questions/list-question/list-question.component";
-// import {EditQuestionComponent} from "./components/questions/edit-question/edit-question.component";
+import {LayoutWithSharedComponent} from './layout-with-shared/layout-with-shared.component';
+import {CategoryModule} from './components/category/category.module';
+import {LoginComponent} from './components/login/login.component';
+import {CreateAccountComponent} from './components/create-account/create-account.component';
+import {QuestionModule} from './components/questions/question.module';
 
 
 // @ts-ignore
