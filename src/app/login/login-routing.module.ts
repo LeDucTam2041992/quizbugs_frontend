264216cac28e55ccdc7674730268c9d8d6d49abc
huy8895 <huy8895@gmail.com>
2020-11-09 22:52:53 +0700

    hoan thanh chuc nang doi mat khau.

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import {RegisterComponent} from "./register/register.component";
 import {HomeComponent} from "./home/home.component";
+import {ChangePasswordComponent} from "./change-password/change-password.component";
+import {AuthGuard} from "../core/auth.guard";
 
 const routes: Routes = [
   {
@@ -20,6 +22,11 @@ const routes: Routes = [
       {
         path: 'register',
         component: RegisterComponent,
+      },
+      {
+        path: 'changePassword',
+        component: ChangePasswordComponent,
+        canActivate: [AuthGuard]
       },
     ]
   }
