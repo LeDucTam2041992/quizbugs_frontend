9eae80111bd6bb717135e15298ddce5ff36dfbd9
Boolshyt <chakeriototo@gmail.com>
2020-11-09 18:28:33 +0700

    hoan thanh Home page giao dien

--- src/app/login/dashboard/dashboard.component.ts
+++ src/app/login/dashboard/dashboard.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   public login = "/login";
   public register ='/register';
+  public home ='/home';
 
   constructor() { }
 
