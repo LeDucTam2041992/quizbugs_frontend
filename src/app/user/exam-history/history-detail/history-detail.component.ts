7956ce6cb6c27e6cdb7be970eacd29efd1645ff9
huy8895 <huy8895@gmail.com>
2020-11-11 20:53:21 +0700

    hien thi danh sach cau hoi cua user da lam trong history-detail.component.html

--- src/app/user/exam-history/history-detail/history-detail.component.ts
+++ src/app/user/exam-history/history-detail/history-detail.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {ExamService} from "../../../service/exam.service";
+import {ActivatedRoute, Router} from "@angular/router";
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {UserExamService} from "../../../service/user-exam.service";
+import {Exam} from "../../../model/exam";
+import {UserExam} from "../../../model/user-exam";
+import {UserAnswer} from "../../../model/user-answer";
 
 @Component({
   selector: 'app-history-detail',
@@ -6,10 +13,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./history-detail.component.scss']
 })
 export class HistoryDetailComponent implements OnInit {
+  currentExam: Exam = {
+    id: 1,
+    name: 'Java Exam',
+    enabled: true,
+    questionSet: []
+  };
+  answersArrMulti = [];
+  answersArr = [];
+  userExamObj: UserExam = {};
+  listUserAnswer: UserAnswer[] = []
 
-  constructor() { }
+  constructor(private examService: ExamService, private route: Router,
+              private router: ActivatedRoute, private fb: FormBuilder,
+              private userExamsService: UserExamService) { }
 
   ngOnInit(): void {
+    const id = Number.parseInt(this.router.snapshot.paramMap.get('id'));
+    this.userExamsService.getUserExamById(id).subscribe(data => {
+      this.currentExam = data.exam;
+      this.answersArr = new Array(this.currentExam.questionSet.length);
+    });
   }
 
 }
