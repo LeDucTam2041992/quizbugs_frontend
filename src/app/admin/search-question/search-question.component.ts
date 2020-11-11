8ab5ddf18e3d3c4297a4617add7e26df90dfc67c
tamleduc <Sakursaoyran204>
2020-11-11 20:36:22 +0700

    tao component search-question

---
+++ src/app/admin/search-question/search-question.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-question',
+  templateUrl: './search-question.component.html',
+  styleUrls: ['./search-question.component.scss']
+})
+export class SearchQuestionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
