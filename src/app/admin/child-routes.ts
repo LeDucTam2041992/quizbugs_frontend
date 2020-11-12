5d3693366ee47922c6d053c4ffb148b370047980
conaldev <talaaidoaibiet@gmail.com>
2020-11-12 15:31:48 +0700

    login guard

--- src/app/admin/child-routes.ts
+++ src/app/admin/child-routes.ts
@@ -1,22 +1,22 @@
 export const childRoutes = [
     {
-    path: 'questions',
-    loadChildren: () => import('./questions/question.module').then(m => m.QuestionModule),
-    data: { icon: 'question_answer', text: 'Question' }
-  },
-  {
-    path: 'categories',
-    loadChildren: () => import('./category/category.module').then(m => m.CategoryModule),
-    data: { icon: 'category', text: 'Category' }
-  },
-  {
-    path: 'quizs',
-    loadChildren: () => import('./quiz/quiz.module').then(m => m.QuizModule),
-    data: { icon: 'insert_drive_file', text: 'Quiz' }
-  },
+        path: 'questions',
+        loadChildren: () => import('./questions/question.module').then(m => m.QuestionModule),
+        data: {icon: 'question_answer', text: 'Question'}
+    },
+    {
+        path: 'categories',
+        loadChildren: () => import('./category/category.module').then(m => m.CategoryModule),
+        data: {icon: 'category', text: 'Category'}
+    },
+    {
+        path: 'quizs',
+        loadChildren: () => import('./quiz/quiz.module').then(m => m.QuizModule),
+        data: {icon: 'insert_drive_file', text: 'Quiz'}
+    },
     {
         path: 'management-user',
-        loadChildren: () => import('./management-user/management-user.module').then(m =>m.ManagementUserModule),
+        loadChildren: () => import('./management-user/management-user.module').then(m => m.ManagementUserModule),
         data: {icon: 'supervisor_account', text: 'Management Users'}
     }
 ];
