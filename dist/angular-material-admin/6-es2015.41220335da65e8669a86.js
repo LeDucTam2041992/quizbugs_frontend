99940a560ef3176f28402841726cb9b2db4ac7f6
conaldev <talaaidoaibiet@gmail.com>
2020-11-12 18:51:24 +0700

    reset form after create-question.component.ts

---
+++ dist/angular-material-admin/6-es2015.41220335da65e8669a86.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{zkoq:function(n,o,t){"use strict";t.d(o,"a",(function(){return e}));var r=t("fXoL"),c=t("FKr1");t("8LU1"),t("cH1L");let e=(()=>{class n{}return n.\u0275mod=r.Jb({type:n}),n.\u0275inj=r.Ib({factory:function(o){return new(o||n)},imports:[[c.j,c.h],c.j,c.h]}),n})()}}]);
